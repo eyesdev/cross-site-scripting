b45fdf6e581f770972501cd40104d3dece2dc848
Jakub <111226326+eyesdev@users.noreply.github.com>
2024-05-15 18:17:37 +0100

    Create aspirin.js

---
+++ aspirin.js
@@ -0,0 +1,11 @@
+<script>
+document.body.style.backgroundColor = "white";
+
+var h1 = document.createElement("h1");
+
+ h1.textContent = "site hacked by reksio fix yo site";
+
+document.body.appendChild(h1);
+
+alert("hello tim can i hide an ip logger into your site");
+<script>
